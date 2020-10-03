22f9e25d3ef7249d79eccb492491fb1d0ee16056
Kerri Sexton <k.sexton804@gmail.com>
2020-10-03 00:32:48 -0400

    currently showing the current day and time at the top of the page

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+
+    var now = moment();
+    $('#currentDay').text(now.format('lll'));
+    // var date = moment().date(Number);
+    // console.log('date is: ', date);
+
+
+})
+
+
+
