448cb3560c25f0313c632dc34ba3bc4ea0d9b4d6
Kerri Sexton <k.sexton804@gmail.com>
2020-10-04 00:01:32 -0400

    gets the current hour, converts to an int and adds 12 if needed to make it 24hr time

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-
+/*
+Task 1
+create a function that uses (.setAttribute) to set css if time is past present of future
+Task 2 
+get the input from the html doc
+Task 3
+eventListener for the button. 
+Task 4
+JSON.stringify/parse get and set to localStorage
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist*/
 
 $(document).ready(function(){
 
     var now = moment();
-    $('#currentDay').text(now.format('lll'));
-    // var date = moment().date(Number);
-    // console.log('date is: ', date);
+    //LLLL is locale support
+    $('#currentDay').text(now.format('LLLL'));
+
+    var currentHour = now.format('hA'); 
+    console.log('Current Hour is: ', currentHour);
+
+    var currentHourInt = parseInt(currentHour);
+    if(currentHourInt <= 5){
+        currentHourInt = currentHourInt + 12;
+    }
+    console.log('Int is: ', currentHourInt);
+
+    var blockArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+    for(var i = 0; i < blockArr.length; i++){
+        if(blockArr[i] < currentHourInt){
+
+        }
+    }
+
 
 
 })
